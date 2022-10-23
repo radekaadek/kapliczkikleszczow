941ba9a2da2061ec5baee0783066b06f71e11128
radekaadek <radekaadek@gmail.com>
2022-10-23 11:25:52 +0200

    test

---
+++ pages/contact.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Contact</h1>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<script>
+export default {
+    head() {
+        return {
+            title: 'Contact',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'This is the contact page'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
