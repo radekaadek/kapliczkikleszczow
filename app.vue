539846c75a7369f53780f65535d301fc6576dcf0
Xertaq <wertez132@gmail.com>
2022-10-23 14:32:35 +0200

    a

--- app.vue
+++ app.vue
@@ -1,5 +1,28 @@
 <template>
     <div>
-        <h1>App</h1>
+        <h1>Contact</h1>
     </div>
 </template>
+
+<script lang="ts"></script>
+
+<script>
+export default {
+    head() {
+        return {
+            title: 'Contact',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'This is the contact page'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
