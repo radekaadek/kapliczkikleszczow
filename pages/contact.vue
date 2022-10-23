a2d2f46b38130b2c229b4a98bcd64730fb286a4a
xKRISTOFx mobile <gral1322@gmail.com>
2022-10-23 16:03:09 +0200

    push

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,1 +1,23 @@
-<Span>DUPA</Span>
+<template>
+    <Span>DUPA</Span>
+</template>
+
+<script lang="ts"></script>
+
+<script>
+export default {
+    head() {
+        return {
+            title: 'KONTAKT',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'This is the contact page',
+                    charset: 'UTF-8'
+                }
+            ]
+        }
+    }
+}
+</script>
