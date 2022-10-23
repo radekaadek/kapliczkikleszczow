1cb28ecc129b3166c5148b18fe5fa2620a9498b3
radekaadek <radekaadek@gmail.com>
2022-10-23 21:12:01 +0200

    zmiany zmiany

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,21 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+    head: {
+        link: [
+            {
+                rel: 'preconnect',
+                href: 'https://fonts.googleapis.com/%22%3E'
+            },
+            {
+                rel: 'preconnect',
+                href: 'https://fonts.gstatic.com/',
+                crossorigin: true
+            },
+            {
+                rel: 'stylesheet',
+                href: 'https://fonts.googleapis.com/css2?family=Fuzzy+Bubbles&display=swap'
+            }
+        ]
+    }
 })
---
+++ pages/twojjebanyfont.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <bar></bar>
+    <h1>Twoj jebany font</h1>
+    <p>Twoj jebany font</p>
+    <button @click="counter++">counter is: {{ counter }}</button>
+  </div>
+</template>
+
+<script>
+var counter = 0;
+export default {
+    data() {
+        return {
+            counter: counter
+        }
+    },
+    methods: {
+        dodaj() {
+            counter++;
+        }
+    }
+}
+</script>
+
+<style>
+h1 {
+    font-family: 'Fuzzy Bubbles', cursive;
+}
+</style>
