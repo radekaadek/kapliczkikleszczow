989af5cc0ac9ee2495aeb9bb7bf7f1d5786d5098
radekaadek <radekaadek@gmail.com>
2022-10-23 12:12:45 +0200

    moze to zadziala

--- app.vue
+++ app.vue
@@ -1,32 +1,5 @@
 <template>
     <div>
         <h1>App</h1>
-        <router-view></router-view>
     </div>
 </template>
-
-<script lang="ts"></script>
-
-<script>
-export default {
-    name: 'App',
-    components: {
-        head() {
-            return {
-                title: 'App',
-                meta: [
-                    {
-                        hid: 'description',
-                        name: 'description',
-                        content: 'App'
-                    }
-                ]
-            }
-        }
-    }
-}
-</script>
-
-<style>
-
-</style>
