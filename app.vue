1a35c3160951a45e53eaae30578187c310a00ecb
radekaadek <radekaadek@gmail.com>
2022-10-23 12:10:30 +0200

    test4

--- app.vue
+++ app.vue
@@ -1,5 +1,32 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+    <div>
+        <h1>App</h1>
+        <router-view></router-view>
+    </div>
 </template>
+
+<script lang="ts"></script>
+
+<script>
+export default {
+    name: 'App',
+    components: {
+        head() {
+            return {
+                title: 'App',
+                meta: [
+                    {
+                        hid: 'description',
+                        name: 'description',
+                        content: 'App'
+                    }
+                ]
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
