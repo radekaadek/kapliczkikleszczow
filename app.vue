b450f8663b3c0b81edd3beac19e15cf611a05102
Xertaq <wertez132@gmail.com>
2022-10-23 15:01:30 +0200

    a

--- app.vue
+++ app.vue
@@ -48,5 +48,78 @@ export default {
 </script>
 
 <style>
-
+* {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    text-decoration: none;
+    box-sizing: border-box;
+}
+body{
+    background-color: white;
+}
+header{
+    width: 100%;
+    height: 100px;
+    background: linear-gradient(to right, #00093c, #2d0b00);
+}
+.inner_header{
+    width: 60%;
+    height: 100%;
+    display: block;
+    margin: 0 auto;
+}
+.logo_container{
+    height: 100%;
+    display: table;
+    float: left;
+}
+.logo_container h1{
+    color: white;
+    height: 100%;
+    display: table-cell;
+    vertical-align: middle;
+    font-family: 'Varela Round', sans-serif;
+    font-size: 40px;
+    font-weight: 800;
+}
+.logo_container h1 span{
+    font-weight: 200;
+}
+.navigation{
+    float: right;
+    height: 100%;
+}
+.navigation a{
+    height: 100%;
+    display: table;
+    float: left;
+    padding: 0px 20px;
+}
+.navigation a:last-child{
+    padding-right: 0;
+}
+.navigation a li{
+    display: table-cell;
+    vertical-align: middle;
+    height: 100%;
+    color: white;
+    font-family: 'Arial';
+    font-size: 16px;
+    text-transform: uppercase;
+}
+main{
+    height: calc(100vh - 100px - 58px);
+}
+iframe{
+    display: flex;
+    width: 100vw;
+    height: 100%;
+} 
+footer{
+    text-align: center;
+    background-color: #333;
+    color: white;
+    padding: 20px;
+}
 </style>
