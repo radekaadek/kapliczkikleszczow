0621664b7339c19a8b26a569a476f5bff4dd9aa8
xKRISTOFx <gral1322@gmail.com>
2022-10-29 20:47:15 +0200

    dodanie i pauza moze reacta sie dolepi

---
+++ components/viewer.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <canvas id="myThreeJsCanvas" />
+    </div>
+</template>
+
+<script>
+import SceneInit from '@/SceneInit.js'
+
+const test = new SceneInit('myThreeJsCanvas');
+test.initialize();
+test.animate();
+let loadedModel;
+const glftLoader = new GLTFLoader();
+
+glftLoader.load('./models/Duck.gltf', (gltfScene) => {
+    loadedModel = gltfScene;
+    gltfScene.scene.rotation.y = Math.PI / 8;
+    gltfScene.scene.position.y = 3;
+    gltfScene.scene.scale.set(10, 10, 10);
+    test.scene.add(gltfScene.scene);
+});
+</script>
---
+++ pages/3d.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <Viewer />
+    </div>
+</template>
+
+<script lang="ts"></script>
+
+<script>
+export default {
+    head() {
+        return {
+            title: 'Kontakt',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'This is the contact page',
+                    charset: 'UTF-8'
+                }
+            ],
+            link: [
+            { rel: 'icon', type: 'image', href: '/static/favicon.svg' }
+          ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
