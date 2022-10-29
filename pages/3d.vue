a30fe47d7b7653ce59119bcf56e1de9c8d029ac4
radekaadek <radekaadek@gmail.com>
2022-10-29 19:27:00 +0200

    Main prawie dziala bez importa sceny

--- pages/3d.vue
+++ pages/3d.vue
@@ -1,27 +1,43 @@
-<!-- create empty vue page -->
 <template>
-    <div>
-        <Bar />
-        <h1>3D</h1>
+  <div>
+      <canvas id="myThreeJsCanvas" />
     </div>
 </template>
 
-<script lang="ts"></script>
-
 <script>
 export default {
-    head() {
-        return {
-            title: '3D',
-            meta: [
-                {
-                    hid: 'description',
-                    name: 'description',
-                    content: 'This is the 3D page',
-                    charset: 'UTF-8'
-                }
-            ]
-        }
-    }
+    
 }
+
+const test = new SceneInit('myThreeJsCanvas');
+    test.initialize();
+    test.animate();
+
+    // const boxGeometry = new THREE.BoxGeometry(8, 8, 8);
+    // const boxMaterial = new THREE.MeshNormalMaterial();
+    // const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+    // test.scene.add(boxMesh);
+
+    let loadedModel;
+    const glftLoader = new GLTFLoader();
+    glftLoader.load('./models/Duck.gltf', (gltfScene) => {
+        loadedModel = gltfScene;
+        // console.log(loadedModel);
+
+        gltfScene.scene.rotation.y = Math.PI / 8;
+        gltfScene.scene.position.y = 3;
+        gltfScene.scene.scale.set(10, 10, 10);
+        test.scene.add(gltfScene.scene);
+    });
+
+    const animate = () => {
+        if (loadedModel) {
+        loadedModel.scene.rotation.x += 0.01;
+        loadedModel.scene.rotation.y += 0.01;
+        loadedModel.scene.rotation.z += 0.01;
+        }
+        requestAnimationFrame(animate);
+    };
+    animate();
+
 </script>
