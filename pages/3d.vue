da8fca2d553eb994b7c34289df156df97a6c10c0
xKRISTOFx <gral1322@gmail.com>
2022-10-31 19:00:23 +0100

    init n b

--- pages/3d.vue
+++ pages/3d.vue
@@ -1,27 +1,28 @@
 <template>
 </template>
 
+
 <script lang="ts"></script>
 
 <script>
-import Vue from 'vue';
-Vue.prototype = window;
-
-new Vue({
-    el: 'root',
-    template: '<Viever/>',
-    components: {Viever}
-});
-// const vm = new Vue({
-//   template: '<Viever />',
-//   created(){
-//     console.log('Created');
-//   },
-//   mounted(){
-//     console.log('Mounted');
-//   }
-// });
-// vm.$mount('root');
+export default {
+    head() {
+        return {
+            title: '3D-ev',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'This is the 3D page',
+                    charset: 'UTF-8'
+                }
+            ],
+            link: [
+            { rel: 'icon', type: 'image', href: '/static/favicon.svg' }
+          ]
+        }
+    }
+}
 </script>
 
 <style>
