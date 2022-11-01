3c0d75267ee08f5b3a55c0cf4602977bf7d0becd
xKRISTOFx mobile <gral1322@gmail.com>
2022-11-01 16:07:13 +0100

    +

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,5 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-    head: {
-        
-    }
+    ssr: false,
 })
--- pages/zglos_kapliczke.vue
+++ pages/zglos_kapliczke.vue
@@ -10,7 +10,6 @@
 
 <script>
 export default {
-    ssr: false,
     head: {
         title: 'Zgłoś kapliczkę',
         link: [
